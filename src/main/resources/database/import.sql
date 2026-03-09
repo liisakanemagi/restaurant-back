d71aa86baedf2c716a2e67c1f9ceb9dbf88d2792
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-09 14:48:14 +0200

    fix: fix restaurant_table coordinates

--- src/main/resources/database/import.sql
+++ src/main/resources/database/import.sql
@@ -1,10 +1,10 @@
 INSERT INTO restaurant_table (table_number, capacity, coordinate_x, coordinate_y, is_window_seat, is_accessible, is_private)
-VALUES ('T1', 6, 15.0, 25.0, true, true, true),
+VALUES ('T1', 6, 15.0, 15.0, true, true, true),
        ('T2', 2, 85.0, 15.0, true, true, true),
        ('T3', 8, 40.0, 45.0, false, true, false),
        ('T4', 6, 65.0, 45.0, false, true, false),
        ('T5', 4, 85.0, 45.0, false, true, false),
        ('T6', 2, 15.0, 70.0, true, false, false),
-       ('T7', 2, 40.0, 70.0, true, false, false),
-       ('T8', 2, 60.0, 70.0, true, false, false),
+       ('T7', 2, 35.0, 70.0, true, false, false),
+       ('T8', 2, 55.0, 70.0, true, false, false),
        ('T9', 4, 85.0, 70.0, true, true, false);
